c067a93b9d46938378b16b9b6ad66a4fd605b0eb
jsprofessional <jshuman@protonmail.com>
2021-07-01 17:09:40 -0500

    added edit functionality

--- profile-page/script.js
+++ profile-page/script.js
@@ -1,1 +1,14 @@
 console.log("page loaded...");
+
+var newName = document.querySelector("#username");
+
+function changeName() {
+    username.innerText = "Not Jane Doe";
+}
+
+// Clicking either x or check icon will remove line from the requests list
+function removeLine(element) {
+    element.remove();
+}
+// Change number of Connection Requests
+// Change number of Your Connections
